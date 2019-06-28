3435dbb7d24b9b218802d51c06711eb2089345af
Jonathan Ray <jonathanray17@gmail.com>
2019-06-28 19:27:34 -0400

    all pins are rendered dynamically instead of the parent map

---
+++ Components/MapPins.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MapView } from 'expo';
+
+function MapPins(props){
+  let userPins = [];
+  Object.keys(props.users).forEach(userId => {
+    // create the array of Markers
+    userPins.push(        
+      <MapView.Marker
+        coordinate={{
+          latitude: props.users[userId].latitude,
+          longitude: props.users[userId].longitude,   
+        }}
+        title={props.users[userId].name}
+        key={userId}
+        // {tagged? description={"TAGGED"} : null}
+      >
+        {/* this is where the player's picture goes */}
+        {props.users[userId].picture ? <Image source={require('./waldo-arrow.png')} style={{width: 30, height: 30,}} />  : null}
+      </MapView.Marker>
+    )
+  })
+  // console.log("userPins: ");
+  // console.log(userPins);
+  return(
+  <>
+    {userPins}
+  </>
+  )
+}
+export default MapPins;
